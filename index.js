0d94255253c58c8fc8d87809e5274a96ac4a06c0
ehasun15019 <ehasunulislam0202@gmail.com>
2026-04-25 22:54:33 +0600

    solved the 39

--- index.js
+++ index.js
@@ -226,4 +226,31 @@ function gropued(arr) {
 // console.log(gropued(users));
 
 
-const intial = 0;
+
+
+
+// question solved: 38 
+
+// Input: nums = [2, 7, 11, 15], 
+// target = 9
+// index return = target
+// Output: [0, 1]
+
+
+function output(nums, target) {
+    const map = {};
+
+    for(let i = 0; i < nums.length; i++) {
+        const current = nums[i];
+        const needs = target - current;
+
+        if(map[needs] !== undefined) {
+            return [map[needs], i]
+        }
+
+        map[current] = i;
+    }
+
+}
+
+console.log(output([2, 7, 11, 15], 9));
