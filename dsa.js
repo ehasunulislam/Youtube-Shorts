59fe9db0d8fe05da53b1f2941fd6d0bea33db834
ehasun15019 <ehasunulislam0202@gmail.com>
2026-04-26 15:22:55 +0600

    complete dsa 2

--- dsa.js
+++ dsa.js
@@ -23,3 +23,36 @@ function output(nums, target) {
 }
 
 // console.log(output([2, 7, 11, 15], 9));
+
+
+
+
+
+
+
+// question solved: 2
+// Input: nums = [1,1,1]   k = 2
+// Output: 2
+
+
+function subArray(nums, k) {
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        let sum = 0;
+
+        for(let j = i; j < nums.length; j++) {
+            sum += nums[j];
+
+            if(sum === k) {
+                count++;
+            }
+        }
+    }
+
+    return count
+}
+
+const nums = [1,1,1, 5, 8, 10];
+const k = 5;
+console.log(subArray(nums, k));
